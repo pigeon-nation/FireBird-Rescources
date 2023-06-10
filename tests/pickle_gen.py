7310994a81ca2c5e565451ca14f259f014aa2400
Pigeon Nation <134137685+pigeon-nation@users.noreply.github.com>
2023-06-11 08:59:55 +1000

    Pickle API now requires .pkls to be in base64

--- tests/pickle_gen.py
+++ tests/pickle_gen.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import pickle
+import base64
 
 def moo():
 	print('moo')
 
 f = open('example_pickle.pkl', 'wb')
 m = pickle.dumps(moo)
-f.write(m)
+f.write(base64.b64encode(m))
 f.close()
