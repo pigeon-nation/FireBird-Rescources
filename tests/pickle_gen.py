56ab5eedc3c920c0ee3f72cfff662bbd42bd1c3a
Pigeon Nation <134137685+pigeon-nation@users.noreply.github.com>
2023-06-11 09:14:39 +1000

    Update pickle_gen.py
    Now deals only with .bpk files

--- tests/pickle_gen.py
+++ tests/pickle_gen.py
@@ -6,7 +6,7 @@ import base64
 def moo():
 	print('moo')
 
-f = open('example_pickle.pkl', 'wb')
+f = open('example_pickle.bpkl', 'w')
 m = pickle.dumps(moo)
-f.write(base64.b64encode(m))
+f.write(base64.b64encode(m).decode())
 f.close()
